0fe59e4607250fb3d2b4393efbfec33ce562f5e8
sahar9776 <sahar.samadi1376@gmail.com>
2024-10-26 22:21:32 +0330

    delete register context from this file

--- context/TotalContext.js
+++ context/TotalContext.js
@@ -1,21 +1,18 @@
 import ShowInfoProvider from "./ShowInfoContext";
 import ComponiesProvider from "./ComponiesContext";
 import ListOfCarsProvider from "./ListOfCarsContext";
-import RegisteredProvider from "./RegisteredContext";
 
 
 function TotalContext({ children }) {
     return (
         <div>
-            <RegisteredProvider>
-                <ListOfCarsProvider>
-                    <ComponiesProvider>
-                        <ShowInfoProvider>
-                            {children}
-                        </ShowInfoProvider>
-                    </ComponiesProvider>
-                </ListOfCarsProvider>
-            </RegisteredProvider>
+            <ListOfCarsProvider>
+                <ComponiesProvider>
+                    <ShowInfoProvider>
+                        {children}
+                    </ShowInfoProvider>
+                </ComponiesProvider>
+            </ListOfCarsProvider>
         </div>
     )
 }
