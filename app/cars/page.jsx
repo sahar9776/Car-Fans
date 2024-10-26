69d27f3e5e651721362ce66839f00c018ff180ed
sahar9776 <sahar.samadi1376@gmail.com>
2024-10-26 21:25:03 +0330

    created cars path/file

---
+++ app/cars/page.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import CarItem from "@/components/CarItem";
+import { ListOfCarsContext } from "@/context/ListOfCarsContext";
+import React, { useContext } from "react";
+
+function Cars() {
+  const { carsInfo } = useContext(ListOfCarsContext);
+
+  return (
+    <div className="w-full py-8 relative">
+      <div className="container mx-auto">
+        {/* text  */}
+        <div className="text-center xl:text-left">
+          <h2 className="h2 mb-6">The 25 Fastest Cars in the World, Ranked </h2>
+          <p className="mb-9 text-white/80">
+            Here's our updated list for 2024, because when marques compete for
+            top-speed bragging rights, the world wins.
+          </p>
+        </div>
+        {/* cars list  */}
+        <div className="columns-1 gap-3 lg:gap-5 lg:columns-2 xl:columns-3">
+          {carsInfo.map((car) => (
+            <CarItem car={car} key={car.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cars;
