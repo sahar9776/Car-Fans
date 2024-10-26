d5315aec3bed15c12d6a3b57ad3e0bd7a3e79bc3
sahar9776 <sahar.samadi1376@gmail.com>
2024-10-26 21:40:40 +0330

    careted desktop nav

---
+++ components/Nav.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+function Nav() {
+  const links = [
+    {
+      name: "home",
+      path: "/",
+    },
+    {
+      name: "cars",
+      path: "/cars",
+    },
+    {
+      name: "componies",
+      path: "/componies",
+    },
+    {
+      name: "racing",
+      path: "/racing",
+    },
+  ];
+  const pathname = usePathname();
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => (
+        <Link
+          href={link.path}
+          key={index}
+          className={`${link.path === pathname && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}
+        >
+          {link.name}
+        </Link>
+      ))}
+    </nav>
+  );
+}
+
+export default Nav;
