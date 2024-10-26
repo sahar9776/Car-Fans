5079a365f5827da6da727fe39e797840fce32bb2
sahar9776 <sahar.samadi1376@gmail.com>
2024-10-26 21:25:39 +0330

    created customization car path/file

---
+++ app/car-custom/[id]/page.jsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { ListOfCarsContext } from "@/context/ListOfCarsContext";
+import { useContext } from "react";
+import { motion } from "framer-motion";
+import CustomOrder from "@/components/CustomOrder";
+
+function CarCustomization({ params }) {
+  const { id } = params;
+  const { carsInfo } = useContext(ListOfCarsContext);
+  const filterCar = carsInfo.filter((item) => item.id === id);
+
+  return (
+    <div className="w-full py-0 lg:py-48">
+      <div className="w-full h-[400px] mx-auto bg-transparent lg:bg-pink-50/20">
+        <div className="container max-auto relative">
+          {filterCar.map((item) => (
+            <div className="" key={item.id}>
+              <motion.img
+                initial={{ y: "-100vh" }}
+                animate={{ y: "0" }}
+                transition={{
+                  delay: 0.8,
+                  duration: 1,
+                  ease: "easeInOut",
+                }}
+                src={item.image}
+                alt=""
+                className="w-full lg:w-[50%] h-auto absolute top-0 lg:-top-36 left-0 lg:left-0"
+              />
+              <motion.img
+                initial={{ y: "100vh" }}
+                animate={{ y: "0" }}
+                transition={{
+                  delay: 0.8,
+                  duration: 1,
+                  ease: "easeInOut",
+                }}
+                src="https://t3.ftcdn.net/jpg/09/30/16/94/360_F_930169493_sk0FPnZLOAywUjQq9gaNeUUE1JH2aUpQ.jpg"
+                alt=""
+                className="w-[40%] h-auto absolute top-48 right-0 "
+              />
+              <motion.h2
+                initial={{ x: "100vw" }}
+                animate={{ x: "0" }}
+                transition={{
+                  delay: 0.8,
+                  duration: 1,
+                  ease: "easeInOut",
+                }}
+                className="h2 max-w-full mt-20 lg:mt-5 text-center absolute top-0 lg:-top-36 left-0 lg:left-[45%]"
+              >
+                {item.carName}
+              </motion.h2>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* custom order  */}
+      <CustomOrder />
+    </div>
+  );
+}
+
+export default CarCustomization;
